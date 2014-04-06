9095138db635972f91f9c606aa3fe07f54fb11b7
Igor Kulman <igor@kulman.sk>
2014-04-06 17:58:08 +0200

    + tests for Windows Phone Store

---
+++ AppStoreCrawler.Tests/WindowsPhoneStore.fs
@@ -0,0 +1,15 @@
+﻿namespace AppStoreCrawler.Tests
+
+open System
+open NUnit.Framework
+open AppStoreCrawler
+
+[<TestFixture>]
+type ``Test crawling Windows Phone Store`` () =
+    
+    [<Test>]
+    member x.``Apps should be gound`` () =
+        let apps = WindowsPhoneStore.searchAppStore "LEMA" "SK"
+        Assert.IsNotNull(apps)
+        Assert.IsNotEmpty(apps)
+
